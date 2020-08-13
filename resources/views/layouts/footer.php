e067d5cade45ac1758cb1f12ac2aef8b81902cf2
Oyekan Oluwatobi <tobainocycle@yahoo.com>
2020-08-13 11:02:07 +0100

    footer

---
+++ resources/views/layouts/footer.php
@@ -0,0 +1,5 @@
+<footer class="page-footer font-small blue">
+<!-- Copyright -->
+<div align="center" style="color: blue"><a href="http://twitter.com/@tobainocycle"> © 2020 All right reserved Action Democratic Part (ADP)  | Sparklegrid </a>
+</div>
+</footer>
