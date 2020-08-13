ade80297a4725262906778ab38ef9101aba1174b
Oyekan Oluwatobi <tobainocycle@yahoo.com>
2020-08-13 10:58:06 +0100

    default string length to fix sql error max key length is 767

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Database\Schema\Builder; // Import Builder where defaultStringLength method is defined
+
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -23,6 +26,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Builder::defaultStringLength(191); // Update defaultStringLength
     }
 }
