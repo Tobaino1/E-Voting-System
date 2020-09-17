ce6df5142cd949711f401ccdcc7571e97fa4917d
Oyekan Oluwatobi <tobainocycle@yahoo.com>
2020-09-17 22:17:11 +0100

    upd on model

--- app/Member.php
+++ app/Member.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Member extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'phone_no',
+        'dob',
+        'gender',
+        'passport_name',
+        'email',
+        'address',
+        'state',
+        'lgalcda',
+        'party_reg_no',
+        'acc_no',
+        
+    ];
 }
