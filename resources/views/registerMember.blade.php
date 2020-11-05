8424b1392c37cb68bf5aa3bc648947f0a333ca62
Oyekan Oluwatobi <tobainocycle@yahoo.com>
2020-11-05 23:59:18 +0100

    modification of member registration

--- resources/views/registerMember.blade.php
+++ resources/views/registerMember.blade.php
@@ -77,7 +77,7 @@
                             </div>
 
 
-                             <div class="form-group row">
+                             <!-- <div class="form-group row">
                                 <label for="passport_name"
                                        class="col-md-4 col-form-label text-md-right">{{ __('Upload Passport Photograph') }}</label>
 
@@ -90,7 +90,7 @@
                                     </span>
                                     @endif
                                 </div>
-                            </div>
+                            </div> -->
 
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
